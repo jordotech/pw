888929904273781b2d3c2e8d170b2e436994d838
jordotech <jaustin@proace.com>
2014-10-18 14:23:30 -0500

    phase one ready

--- sites/all/themes/promise/js/promise.js
+++ sites/all/themes/promise/js/promise.js
@@ -1,18 +1,39 @@
 var $ = jQuery.noConflict();
 
 $(document).ready(function () {
-
     $('.close-button').click(function () {
         $(this).hide();
         $('.grid-item').removeClass('dark');
         $('#details .drop-in').empty();
         $('.grid-item-img').show();
     });
+    $('.grid-item-img').mouseenter(function(){
+        $(this).find('.grid-img-overlay').animate({
+            opacity: ".7"
+        }, 500, function () {
+        });
+    });
+    $('.grid-item-img').mouseleave(function(){
+        $(this).find('.grid-img-overlay').animate({
+            opacity: "0"
+        }, 500, function () {
+        });
+    });
+
+    $('#controls li').click(function(){
+        var tid = $(this).attr('tid');
+
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.grid-item').show();
+        if($(this).attr('tid') != 'all'){
+            $('.grid-item').not('[tid="' + tid + '"]').hide();
+
+        }
+
+    });
 });
 
 function itemDetail(nid) {
-
-
     var item = $('.grid-item[nid="' + nid + '"]');
     if(!$(item).hasClass('dark')){
         $('.close-button').show();
@@ -21,7 +42,8 @@ function itemDetail(nid) {
         $(body).appendTo('#details .drop-in');
         $(item).find('.grid-item-img').hide();
     }
+}
 
-
-
+pw = {
+    'hello':'bye'
 }
